b61f3af6978589e8f68258a4fc2b02c8782d84f3
Rogerio Peyroton <rogeriobp@gmail.com>
2016-11-21 21:34:31 -0200

    Created timer component

--- app/components/Timer.jsx
+++ app/components/Timer.jsx
@@ -1,11 +1,66 @@
 var React = require('react');
+var Clock = require('Clock');
+var Controls = require('Controls');
 
 var Timer = React.createClass ({
-  render: function() {
+  getInitialState: function () {
+    return {
+      count: 0,
+      countdownStatus: 'stopped'
+    }
+  },
+  componentDidUpdate: function(prevProps, prevState) {
+    if (this.state.countdownStatus !== prevState.countdownStatus) {
+      switch (this.state.countdownStatus) {
+        case 'started':
+          this.startTimer();
+          break;
+        case 'stopped':
+          this.setState({count:0});
+        case 'paused':
+          clearInterval(this.timer);
+          this.timer = undefined;
+          break;
+      }
+    }
+  },
+  componentWillUnmount: function () {
+    clearInterval(this.timer);
+    this.timer = undefined;
+  },
+  startTimer: function() {
+    this.timer = setInterval(() => {
+      var newCount = this.state.count + 1;
+      this.setState({
+        count: newCount
+      });
+    }, 1000);
+    this.setState({countdownStatus: 'started'});
+  },
+  handleStatusChange: function (newStatus) {
+    this.setState({countdownStatus: newStatus});
+  },
+  handleStartTimer: function () {
+    this.setState({countdownStatus: 'started'});
+  },
+  render: function () {
+    var {count, countdownStatus} = this.state;
+    var renderControlArea = () => {
+      if (countdownStatus === 'stopped') {
+        return <button className="button expanded" onClick={this.handleStartTimer}>Start</button>
+      } else {
+        return <Controls countdownStatus={countdownStatus} onStatusChange={this.handleStatusChange}/>
+      }
+    }
+
     return(
-      <p>Timer.jsx</p>
-    );
+      <div>
+        <h1 className="page-title">Timer App</h1>
+        <Clock totalSeconds={count}/>
+        {renderControlArea()}
+      </div>
+    )
   }
-});
+})
 
 module.exports = Timer;
